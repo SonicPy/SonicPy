a3d4d867d85e20a98101cb2d01921f4bb2583bea
Ross-home-mac <hrubiak@gmail.com>
2019-11-20 19:39:40 -0600

    Update UltrasoundAnalysis.py

--- UltrasoundAnalysis.py
+++ UltrasoundAnalysis.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from numpy import argmax, nan, greater,less, append, sort, array
 import sys
 from PyQt5 import QtWidgets
 import pyqtgraph as pg
@@ -18,11 +19,12 @@ from widgets.PltWidget import SimpleDisplayWidget, customWidget
 from widgets.CustomWidgets import HorizontalSpacerItem
 
 from scipy import optimize
+from scipy.signal import argrelextrema
 
 def load_file():
     filename = open_file_dialog(None, "Load File(s).", None) 
     if len(filename):
-        t, spectrum = read_tek_csv(filename, subsample=1)
+        t, spectrum = read_tek_csv(filename, subsample=4)
         t, spectrum = zero_phase_highpass_filter([t,spectrum],1e4,1)
         return t,spectrum
     else:
@@ -144,23 +146,116 @@ def update_data():
 def fit_func(x, a, b,c,d):
     return a * np.cos(b * x+c)+d
 
-def calculate_data():
-    c1 = plot_win_detail1.get_cursor_pos()
-    c2 = plot_win_detail2.get_cursor_pos()
 
-    c_diff = c2-c1
-    cor_range = 800
+def index_of_nearest(values, value):
+    items = []
+    for ind, v in enumerate(values):
+        diff = abs(v-value)
+        item = (diff, v, ind)
+        items.append(item)
+    def getKey(item):
+        return item[0]
+    s = sorted(items, key=getKey)
+    closest = s[0][1]
+    closest_ind = s[0][2]
+    return closest_ind
+
+def get_local_optimum(x, xData, yData, optima_type=None):
+
+    if len(xData) and len(yData):
+        pind = get_partial_index(xData,x)
+        if pind is None:
+            return None
+        pind1 = int(pind-100)
+        pind2 = int(pind +100)
+        xr = xData[pind1:pind2]
+        
+        yr = yData[pind1:pind2]
+        # for local maxima
+        maxima_ind = argrelextrema(yr, greater)
+        maxima_x = xr[maxima_ind]
+        maxima_y = yr[maxima_ind]
+
+        minima_ind = argrelextrema(yr, less)
+        minima_x = xr[minima_ind]
+        minima_y = yr[minima_ind]
+
+        if optima_type == 'minimum':
+            optima_pind = index_of_nearest(minima_x,x)
+            optima_x = minima_x[optima_pind]
+            optima_x = array([optima_x])
+            optima_y = array([minima_y[optima_pind]])
+
+        if optima_type == 'maximum':
+            optima_pind = index_of_nearest(maxima_x,x)
+            optima_x = maxima_x[optima_pind]
+            optima_x = array([optima_x])
+            optima_y = array([maxima_y[optima_pind]])
+        
+        if optima_type is None:
+            optima_ind = sort(append(maxima_ind, minima_ind))
+            optima_x = xr[optima_ind]
+            optima_y = yr[optima_ind]
+            optima_pind = int(round(get_partial_index(optima_x,x)))
+            optima_x = optima_x[optima_pind]
+            if optima_x in minima_x:
+                optima_type='minimum'
+            if optima_x in maxima_x:
+                optima_type='maximum'
+            optima_x = array([optima_x])
+            optima_y = array([optima_y[optima_pind]])
+        
+        print (optima_x)
+        print (optima_type)
+        return (optima_x, optima_y), optima_type
+
+def snap_cursors_to_optimum(c1, c2, t, spectrum):
+    
+    cor_range = 50
     
     pilo1 = int(get_partial_index(t,c1))-int(cor_range/2)
     pihi1 = int(get_partial_index(t,c1))+int(cor_range/2)
-    picenter2 = int(get_partial_index(t,c2))
+    
+    pilo2 = int(get_partial_index(t,c2))-int(cor_range/2)
+    pihi2 = int(get_partial_index(t,c2))+int(cor_range/2)
     
 
     slice1 = np.asarray(spectrum)[pilo1:pihi1]
     #pg.plot(np.asarray(slice1), title="slice1")
     t1 = np.asarray(t)[pilo1:pihi1]
+    slice2 = np.asarray(spectrum)[pilo2:pihi2]
+    #pg.plot(np.asarray(slice1), title="slice1")
+    t2 = np.asarray(t)[pilo2:pihi2]
 
-    shift_range = 200
+    (optima_x_1, optima_y_1), optima_type_1 = get_local_optimum (c1, t1, slice1)
+    (optima_x_2, optima_y_2), optima_type_2 = get_local_optimum (c2, t2, slice2)
+    return optima_x_1, optima_x_2, optima_y_1, optima_y_2
+
+
+def calculate_data():
+
+    c1 = plot_win_detail1.get_cursor_pos()
+    c2 = plot_win_detail2.get_cursor_pos()
+
+    optima_x_1, optima_x_2, optima_y_1, optima_y_2 = snap_cursors_to_optimum(c1, c2,t, spectrum)
+
+    plot_win_detail1.set_cursor_pos(optima_x_1[0])
+    plot_win_detail2.set_cursor_pos(optima_x_2[0])
+
+    c1 = plot_win_detail1.get_cursor_pos()
+    c2 = plot_win_detail2.get_cursor_pos()
+
+    cor_range = 250
+    
+    pilo1 = int(get_partial_index(t,c1))-int(cor_range/2)
+    pihi1 = int(get_partial_index(t,c1))+int(cor_range/2)
+    picenter2 = int(get_partial_index(t,c2))
+
+    slice1 = np.asarray(spectrum)[pilo1:pihi1]
+    #pg.plot(np.asarray(slice1), title="slice1")
+    t1 = np.asarray(t)[pilo1:pihi1]
+  
+    shift_range = 30
     cross_corr = []
     for shift in range(shift_range):
         pilo2 = picenter2 - int(cor_range/2) + int((shift-shift_range/2))
@@ -171,9 +266,12 @@ def calculate_data():
 
     
     y_data=np.asarray(cross_corr)
-    n = range(len(cross_corr))
-    params, params_covariance = optimize.curve_fit(fit_func, n, y_data,p0=[.5,.02,.1, .1])
+    n = np.asarray(range(len(cross_corr)))
+    params, params_covariance = optimize.curve_fit(fit_func, n, y_data,p0=[.99,.011,.1, .1])
     fitted = fit_func(n, params[0], params[1], params[2], params[3])
+
+    #pg.plot(np.asarray(cross_corr), title="cross_corr")
+    #pg.plot(np.asarray(fitted), title="fit")
 
     #print (params)
     a = params[0]
@@ -183,43 +281,50 @@ def calculate_data():
     neg_factor = 1
     max_found = False
     
-    
+    print (params)
     if a >= 0:
-        for n in range(5):
-            x_max = (2*np.pi * (n-2) -c )/b
+        for n in range(15):
+            x_max = (2*np.pi * (n-7) -c )/b
             if x_max >= 0 and x_max <= len(cross_corr):
-                #print(x_max)
+                print(x_max)
+                print(n)
                 max_found = True
                 break
         if max_found:
             p_val = x_max - shift_range/2
         else:
-            for n in range(5):
-                x_max = (2*np.pi *(n-2) -c + np.pi)/b
+            for n in range(15):
+                x_max = (2*np.pi *(n-7) -c + np.pi)/b
                 if x_max >= 0 and x_max <= len(cross_corr):
-                    #print(x_max)
+                    print(x_max)
+                    print(n)
                     neg_factor = -1
                     break
             p_val = x_max - shift_range/2
 
     else:
-        for n in range(5):
-            x_max = (2*np.pi *(n-2) -c + np.pi)/b
+        for n in range(15):
+            x_max = (2*np.pi *(n-7) -c + np.pi)/b
             if x_max >= 0 and x_max <= len(cross_corr):
-                #print(x_max)
+                print(x_max)
+                print(n)
                 max_found = True
                 break
         if max_found:
             p_val = x_max - shift_range/2
         else:
-            for n in range(5):
-                x_max = (2*np.pi * (n-2) -c )/b
+            for n in range(15):
+                x_max = (2*np.pi * (n-7) -c )/b
                 if x_max >= 0 and x_max <= len(cross_corr):
-                    #print(x_max)
+                    print(x_max)
+                    print(n)
                     neg_factor = -1
                     break
             p_val = x_max - shift_range/2
         
+    if not max_found:
+        print(x_max)
+        print(n)
     t_step = t[1]-t[2]
     t_max_shift = p_val * t_step
 

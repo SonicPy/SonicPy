0b326cf2f9b19369f8c762c1c53ddcfb7ecf5b55
Ross-home-mac <hrubiak@gmail.com>
2020-08-17 23:27:11 -0500

    Update UltrasoundMeasurement.py

--- UltrasoundMeasurement.py
+++ UltrasoundMeasurement.py
@@ -1,8 +1,7 @@
 # TODO:
-#  arb waveforms corect frequency display
-#  plot interaction (box zoom), 
-#     cursors (buttons to toggle on/off each cursor), 
-#     oveview plot and zoom plot 
+#  waterfall plots
+
+# 
 
 
 from um import main

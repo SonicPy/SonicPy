fee226b5736cf50ab1d36592d148780a231508b6
Ross Hrubiak <hrubiak@gmail.com>
2022-05-20 10:39:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'R. Hrubiak'
 
 # The full version, including alpha/beta/rc tags
 release = '0.5.5'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 

6705f6bf6c47fe7a3df4fb5f1851a0ef82d191e5
Ross Hrubiak <hrubiak@gmail.com>
2022-06-22 22:35:02 -0500

    Update EchoesResultsModel.py

--- ua/models/EchoesResultsModel.py
+++ ua/models/EchoesResultsModel.py
@@ -87,79 +87,35 @@ class EchoesResultsModel():
     def load_result_from_file(self, ):
 
         for subfolder in self.subfolders:
-            p_folder = os.path.join(self.folder, subfolder, 'P')
-            p_exists = os.path.exists(p_folder)
-            if p_exists:
-                json_search = os.path.join(p_folder,'*.json')
-                p_res_files = glob.glob(json_search)
-                for p_file in p_res_files:
+            wave_types = ['P','S']
+            for wave_type in wave_types:
+                folder = os.path.join(self.folder, subfolder, wave_type)
+                exists = os.path.exists(folder)
+                if exists:
+                    json_search = os.path.join(folder,'*.json')
+                    res_files = glob.glob(json_search)
+                    for file in res_files:
 
-                    with open(p_file) as json_file:
-                        data = json.load(json_file)
-                        correlation = data
-                        if 'wave_type' in correlation:
-                            wave_type = correlation['wave_type']
-                            if wave_type == 'P':
-                                if 'filename_waweform' in correlation:
-                                    saved_path = correlation['filename_waweform']
-                                elif 'filename_waveform' in correlation:
-                                    saved_path = correlation['filename_waveform']
-                                else:
-                                    continue
-                                saved_path = os.path.normpath(saved_path)
-                                file_split = os.path.split(saved_path)[-1]
+                        with open(file) as json_file:
+                            data = json.load(json_file)
+                            correlation = data
+                            if 'wave_type' in correlation:
+                                wave_type = correlation['wave_type']
+                                if wave_type == wave_type:
+                                    if 'filename_waweform' in correlation:
+                                        saved_path = correlation['filename_waweform']
+                                    elif 'filename_waveform' in correlation:
+                                        saved_path = correlation['filename_waveform']
+                                    else:
+                                        continue
+                                    saved_path = os.path.normpath(saved_path)
+                                    file_split = os.path.split(saved_path)[-1]
 
-                                
-                                base_folder = os.path.split(os.path.split(saved_path)[0])[-1]
-                                updated_path = os.path.join(self.folder, base_folder, file_split)
-                                correlation['filename_waveform'] = updated_path
-                                self.add_echoe(correlation)
-                        json_file.close()
+                                    
+                                    base_folder = os.path.split(os.path.split(saved_path)[0])[-1]
+                                    updated_path = os.path.join(self.folder, base_folder, file_split)
+                                    correlation['filename_waveform'] = updated_path
+                                    self.add_echoe(correlation)
+                            json_file.close()
 
-            s_folder = os.path.join(self.folder, subfolder, 'S')
-            s_exists = os.path.exists(s_folder)
-            if s_exists:
-                json_search = os.path.join(s_folder,'*.json')
-                s_res_files = glob.glob(json_search)
-                for s_file in s_res_files:
-
-                    with open(s_file) as json_file:
-                        data = json.load(json_file)
-                        correlation = data
-                        if 'wave_type' in correlation:
-                            wave_type = correlation['wave_type']
-                            if wave_type == 'S':
-                                if 'filename_waweform' in correlation:
-                                    saved_path = correlation['filename_waweform']
-                                elif 'filename_waveform' in correlation:
-                                    saved_path = correlation['filename_waveform']
-                                else:
-                                    continue
-                                saved_path = os.path.normpath(saved_path)
-                                file_split = os.path.split(saved_path)[-1]
-
-                                
-                                base_folder = os.path.split(os.path.split(saved_path)[0])[-1]
-                                updated_path = os.path.join(self.folder, base_folder, file_split)
-                                correlation['filename_waveform'] = updated_path
-                                self.add_echoe(correlation)
-                        json_file.close()
-                        
-
-        '''if filename.endswith('.json'):
-            with open(filename, 'r') as json_file:
-                correlations = json.load(json_file)   
-                self.clear()
-                echoes_p = correlations['P']
-                echoes_s = correlations['S']
-                
-                for fname in echoes_p:
-                    
-                    echo = echoes_p[fname]
-                    self.add_echoe(echo)
-                for fname in echoes_s:
-                    
-                    echo = echoes_s[fname]
-                    self.add_echoe(echo)
-                json_file.close()'''
-        
+            

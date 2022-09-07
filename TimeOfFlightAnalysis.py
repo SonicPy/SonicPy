181eb2876df95722350f1ba872bd827a758da7f7
Ross-mac-work <hrubiak@gmail.com>
2022-09-07 15:35:21 -0500

    Update TimeOfFlightAnalysis.py

--- TimeOfFlightAnalysis.py
+++ TimeOfFlightAnalysis.py
@@ -1,18 +1,11 @@
 
 # TODO hdf5 (data measurement and analysis)
-
 # TODO add color for already selected echoes
-
 # TODO project file
-
 # TODO save echoes per frequency in file dict
-
 # TODO cycle through all frequencies automatically
-
 # TODO broad pulse 
-
 # TODO fix arrow plot updating
-
 
 from ua import TOF
 

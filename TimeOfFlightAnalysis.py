09a01bf1be9cb4c05341cdaa41d3afda6c910bc5
Ross-home-mac <hrubiak@gmail.com>
2022-09-19 18:42:02 -0500

    Update TimeOfFlightAnalysis.py

--- TimeOfFlightAnalysis.py
+++ TimeOfFlightAnalysis.py
@@ -1,22 +1,11 @@
 
 # TODO hdf5 (data measurement and analysis)
-#  add color for already selected echoes
-#  project file
-#  save echoes per frequency in file dict
-#  cycle through all frequencies automatically
-#  broad pulse 
-#  fix arrow plot updating
-#  Vp and Vs output into spreadsheet
+# TODO broadband pulse 
 # TODO plots/data output for publication
 # TODO selecting Vp/Vs in the results table
 # TODO save metadata in um, read metadata in ua
-#  json project file
-#  save sorted folders
-#  save settings in project file
 # TODO toggle echo show/hide in overview widget
 # TODO sort output folders
-#  close and clear project
-
 
 from ua import TOF
 

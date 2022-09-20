1154ca6992c04b53538def200e3eef826e5a2e23
Ross-home-mac <hrubiak@gmail.com>
2022-09-19 20:42:07 -0500

    na

--- TimeOfFlightAnalysis.py
+++ TimeOfFlightAnalysis.py
@@ -1,10 +1,11 @@
 
+# TODO broadband pulse 
+# TODO work with single spectrum
+# TODO toggle echo show/hide in overview widget
+# TODO plots/data output for publication
+# TODO sort output folders
 # TODO hdf5 (data measurement and analysis)
 # TODO save metadata in um, read metadata in ua
-# TODO broadband pulse 
-# TODO plots/data output for publication
-# TODO toggle echo show/hide in overview widget
-# TODO sort output folders
 
 from ua import TOF
 
